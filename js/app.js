dd10e01523e86b18734aad9b47e2a6e86e3e76b7
Supriya-debnath <supriya.cucse@gmail.com>
2021-09-13 18:20:45 +0600

    update evaly store

--- js/app.js
+++ js/app.js
@@ -11,19 +11,20 @@ const showProducts = (products) => {
   const allProducts = products.map((pd) => pd);
   for (const product of allProducts) {
     console.log(product);
-    const image = product.images;
+    const image = product.image;
     const div = document.createElement("div");
-    div.classList.add("product");
-    div.innerHTML = `<div class="single-product">
-      <div>
-    <img class="product-image" src=${product.image}></img>
-      </div>
-      <h3>${product.title}</h3>
-      <p>Category: ${product.category}</p>
+    div.classList.add("product,p-5");
+    div.innerHTML = `<div class="card border-1 m-3 p-3 shadow single-product" style="height:540px">
+      <img class="product-image mx-auto" src=${image}></img>
+      <div class="card-body">
+      <h3 class="card-title">${product.title}</h3>
+      <p class="card-text">Category: ${product.category}</p>
       <p>Rate: ${product.rating.rate}, Count: ${product.rating.count}</p>
       <h2>Price: $ ${product.price}</h2>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button></div>
+      <button id="details-btn" class="btn btn-danger">Details</button>
+      </div>
+      </div>
       `;
       document.getElementById("all-products").appendChild(div);
   }
@@ -33,14 +34,14 @@ const addToCart = (id, price) => {
   count = count + 1;
   updatePrice("price", price);
   
-  updateTotal();
+  
   updateTaxAndCharge();
   document.getElementById("total-Products").innerText = count;
 };
 
 const getInputValue = (id) => {
   const element = document.getElementById(id).innerText;
-  const converted = parseInt(element);
+  const converted = parseFloat(element);
   return converted;
 };
 
@@ -72,6 +73,7 @@ const updateTaxAndCharge = () => {
     setInnerText("delivery-charge", 60);
     setInnerText("total-tax", priceConverted * 0.4);
   }
+    updateTotal();
 };
 
 //grandTotal update function
